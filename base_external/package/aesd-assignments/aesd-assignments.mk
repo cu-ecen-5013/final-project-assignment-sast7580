9e7759f47cbe205b902ddd7ce1a5cb1422c721b8
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-10-09 23:44:21 -0600

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4bcb48b5cdf6cb2860d14798d11f709d0aed89c3
+AESD_ASSIGNMENTS_VERSION = 002d95de5ad229d8dfb0b72bd2cce02deb49a1ff
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/aesd-assignments-3-and-later-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -20,6 +20,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/sockettest.sh $(TARGET_DIR)/bin
 endef
 
 
