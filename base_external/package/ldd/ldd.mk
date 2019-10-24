550db8e952948821ecfcdd2683a4f2bf69d70b69
Rushi Macwan <rushi.macwan@colorado.edu>
2019-10-23 22:11:04 -0600

    updates

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 LDD_VERSION = f406ae89f022e0609bb3a4323607c4477670ca88
-LDD_SITE = git@github.com:cu-ecen-5013/assignment-7-buildroot-and-yocto-kernel-builds-MacRush7.git/misc-modules
+LDD_SITE = git@github.com:cu-ecen-5013/assignment-7-buildroot-and-yocto-kernel-builds-MacRush7.git
 LDD_SITE_METHOD = git
 LDD_MODULE_SUBDIRS = misc-modules
 
