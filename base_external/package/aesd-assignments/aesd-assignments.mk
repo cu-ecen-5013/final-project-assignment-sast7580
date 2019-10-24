77b3d188dbd82e6b568c4206c2e13a6ae89b4354
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-10-23 23:53:20 -0600

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 6fd892bdd7bfcd6f826a6d1c0e101c1ca625f01f
+AESD_ASSIGNMENTS_VERSION = 5c65898ba3726aa2e85fcc0d2498a52e0b112288
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/aesd-assignments-3-and-later-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
