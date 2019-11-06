017280eb42de1967cb3a8ec2eec84817291e5daf
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-11-06 10:04:13 -0700

    Update ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,15 +5,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_MODULE_VERSION = f406ae89f022e0609bb3a4323607c4477670ca88
+LDD_VERSION = 5065f15fad0ec608558accdff2e4694d6e8aa6ce
 LDD_SITE = git@github.com:cu-ecen-5013/assignment-7-buildroot-and-yocto-kernel-builds-MacRush7.git
 LDD_SITE_METHOD = git
 LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
-
-define KERNEL_MODULE_BUILD_CMDS
-        $(MAKE) -C '$(@D)' LINUX_DIR='$(LINUX_DIR)' CC='$(TARGET_CC)' LD='$(TARGET_LD)' modules
-endef
  
 $(eval $(kernel-module))
 $(eval $(generic-package))
