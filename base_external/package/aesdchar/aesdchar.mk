7995a0b2c5985d5c4be0339d3eac6ac8d945e188
Rushi Macwan <rushi.macwan@colorado.edu>
2019-11-11 21:33:03 -0700

    updates

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -8,6 +8,7 @@
 
 AESDCHAR_VERSION = 7ac45c925e5170f99cbf6268492f3adec620a098
 AESDCHAR_SITE = git@github.com:cu-ecen-5013/aesd-assignments-3-and-later-MacRush7.git
+
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver/
  
