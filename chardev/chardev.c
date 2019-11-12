03ac91802214ea1832dc9954957d73cd7967c9de
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-11-12 12:28:44 -0700

    Create chardev.c

---
+++ chardev/chardev.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void);
+static int hello_exit(void);
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+static int hello_init(void)
+{
+  printk(KERN_ALERT "Hello World!\n");
+  return 0;
+}
+
+static int hello_exit(void)
+{
+  printk(KERN_ALERT "Goodbye, cruel world\n");
+}
