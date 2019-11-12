bd9e32fa00015514ea074b6fe6be89877946682c
Rushi Macwan <rushi.macwan@colorado.edu>
2019-11-11 22:04:22 -0700

    updates

--- aesd-char-driver/aesdchar.c
+++ aesd-char-driver/aesdchar.c
@@ -10,18 +10,6 @@
  * @copyright Copyright (c) 2019
  *
  */
- 
-/**
- * External resources utilized to complete this file are as under:
- *
- * Source-1: http://derekmolloy.ie/writing-a-linux-kernel-module-part-2-a-character-device/
- * Source-2: https://www.geeksforgeeks.org/linux-kernel-module-programming-hello-world-program/
- * Source-3: https://github.com/cu-ecen-5013/ldd3-samples.git
- */
- 
-//////////////////////////////////////////////////////
-// Header Files
-//////////////////////////////+////////////////////////
 
 #include <linux/module.h>
 #include <linux/init.h>
@@ -29,82 +17,31 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/uaccess.h>          // Required for the copy to user function
-#include <linux/slab.h>
 #include "aesdchar.h"
-
-//////////////////////////////////////////////////////
-// Global variables & definitions
-//////////////////////////////+////////////////////////
 
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Rushi Macwan"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Rushi James Macwan"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
 
-//////////////////////////////////////////////////////
-// Additional global variables
-//////////////////////////////+////////////////////////
-
-static int device_open_cnt = 0; 	// Device open instances
-//static char data_buf[1024] = {0}; 	// Limited size device_data buffer during this edition
-static char *data_buf;
-//static short size_of_data_buf;		// Size of the data_buf buffer
-
-//////////////////////////////////////////////////////
-// Module function prototypes and structures
-//////////////////////////////+////////////////////////
-
-int 		aesd_open(struct inode *inode, struct file *filp);
-int 		aesd_release(struct inode *inode, struct file *filp);
-ssize_t 	aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
-ssize_t 	aesd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
-static int 	aesd_setup_cdev(struct aesd_dev *dev);
-
-struct file_operations aesd_fops = {
-	.owner =    THIS_MODULE,
-	.read =     aesd_read,
-	.write =    aesd_write,
-	.open =     aesd_open,
-	.release =  aesd_release,
-};
-
-//////////////////////////////////////////////////////
-// Module function definitions
-//////////////////////////////+////////////////////////
-
 int aesd_open(struct inode *inode, struct file *filp)
 {
 	PDEBUG("open");
-	
-	//////////////////////////////////////////////////////
-	// aesd_open handling
-	//////////////////////////////////////////////////////
-	
-	device_open_cnt++;
-	//printk(KERN_INFO "/dev/aesdchar: Device has been opened %d time(s)\n", device_open_cnt);
-	struct aesd_dev *dev; /* device information */
-	dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
-	filp->private_data = dev; /* for other methods */
-	
+	/**
+	 * TODO: handle open
+	 */
 	return 0;
 }
 
 int aesd_release(struct inode *inode, struct file *filp)
 {
 	PDEBUG("release");
-	
-	//////////////////////////////////////////////////////
-	// aesd_release handling
-	//////////////////////////////////////////////////////
-	
-	//printk(KERN_INFO "/dev/aesdchar: Device has been closed\n");
-	
+	/**
+	 * TODO: handle release
+	 */
 	return 0;
 }
 
@@ -113,24 +50,10 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 {
 	ssize_t retval = 0;
 	PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
-	
-	//////////////////////////////////////////////////////
-	// aesd_read handling
-	//////////////////////////////////////////////////////
-
-	struct aesd_dev *dev = filp->private_data; 
-
-	if (copy_to_user(buf, data_buf, count)) 
-	{
-		retval = -EFAULT;
-		goto out;
-	}
-	
-	*f_pos += count;
-	retval = count;
-	
-  out:
-	return retval;	
+	/**
+	 * TODO: handle read
+	 */
+	return retval;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
@@ -138,25 +61,18 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
 {
 	ssize_t retval = -ENOMEM;
 	PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
-	
-	//////////////////////////////////////////////////////
-	// aesd_write handling
-	//////////////////////////////////////////////////////
-	
-	struct aesd_dev *dev = filp->private_data;
-	
-	if (copy_from_user(data_buf, buf, count)) 
-	{
-		retval = -EFAULT;
-		goto out;
-	}
-	
-	*f_pos += count;
-	retval = count;
-	
-  out:
+	/**
+	 * TODO: handle write
+	 */
 	return retval;
 }
+struct file_operations aesd_fops = {
+	.owner =    THIS_MODULE,
+	.read =     aesd_read,
+	.write =    aesd_write,
+	.open =     aesd_open,
+	.release =  aesd_release,
+};
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
 {
@@ -172,16 +88,11 @@ static int aesd_setup_cdev(struct aesd_dev *dev)
 	return err;
 }
 
-//////////////////////////////////////////////////////
-// Module init and exit
-//////////////////////////////+////////////////////////
+
 
 int aesd_init_module(void)
 {
-	printk(KERN_INFO "/dev/aesdchar: Initializing the aesdchar module\n");
-	
-	dev_t dev;
-	dev = 0;
+	dev_t dev = 0;
 	int result;
 	result = alloc_chrdev_region(&dev, aesd_minor, 1,
 			"aesdchar");
@@ -192,13 +103,10 @@ int aesd_init_module(void)
 	}
 	memset(&aesd_device,0,sizeof(struct aesd_dev));
 
-	//////////////////////////////////////////////////////
-	// Driver initialization
-	//////////////////////////////////////////////////////
-	
-	data_buf = kmalloc(1024, GFP_KERNEL);
-	
-	
+	/**
+	 * TODO: initialize the AESD specific portion of the device
+	 */
+
 	result = aesd_setup_cdev(&aesd_device);
 
 	if( result ) {
@@ -214,15 +122,14 @@ void aesd_cleanup_module(void)
 
 	cdev_del(&aesd_device.cdev);
 
-	//////////////////////////////////////////////////////
-	// Driver cleanup
-	//////////////////////////////////////////////////////
-
-	kfree(data_buf);
-	
+	/**
+	 * TODO: cleanup AESD specific poritions here as necessary
+	 */
 
 	unregister_chrdev_region(devno, 1);
 }
 
-	module_init(aesd_init_module);
-	module_exit(aesd_cleanup_module);
+
+
+module_init(aesd_init_module);
+module_exit(aesd_cleanup_module);
