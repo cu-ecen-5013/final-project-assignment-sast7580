3b2f39e4eaf01345e50ee0de5f462a8456b2df64
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-11-12 13:23:29 -0700

    Update chardev.c

--- chardev/chardev.c
+++ chardev/chardev.c
@@ -6,9 +6,6 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int hello_init(void);
 static int hello_exit(void);
 
-module_init(hello_init);
-module_exit(hello_exit);
-
 static int hello_init(void)
 {
   printk(KERN_ALERT "Hello World!\n");
@@ -20,3 +17,6 @@ static int hello_exit(void)
   printk(KERN_ALERT "Goodbye, cruel world\n");
   return 0;
 }
+
+module_init(hello_init);
+module_exit(hello_exit);
