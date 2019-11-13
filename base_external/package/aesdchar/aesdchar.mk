9c0d6fb87847a707ba312bd6bee39d840d67e0f7
Rushi Macwan <rushi.macwan@colorado.edu>
2019-11-12 17:00:36 -0700

    updates

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 
-AESDCHAR_VERSION = master
+AESDCHAR_VERSION = a07ef67b9959ce6aa4c8f496bae1d4872bee9f80
 AESDCHAR_SITE = git@github.com:MacRush7/aesdchar.git
 
 #AESDCHAR_VERSION = 7ac45c925e5170f99cbf6268492f3adec620a098
