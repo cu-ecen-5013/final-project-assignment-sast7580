2bc22a8f22768655d5558e3d9c9f71d04ed257f1
Sarah Stephany <sast7580@colorado.edu>
2019-12-05 10:11:08 -0700

    update to use final folder

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,9 +5,14 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 6a8d5cdb75a1dd58ea9f98550261e3335bfaa33f
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/aesd-assignments-3-and-later-MacRush7.git
+AESD_ASSIGNMENTS_VERSION = 2660ab430c275ba5bf1ce5510aaf3974be9c2499
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = buzzer-driver/
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = fingerprint-driver/
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = gsm-driver/
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = rfid-driver/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -18,11 +23,11 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/S98lddmodules.sh $(TARGET_DIR)/etc/init.d/S98lddmodules
+	$(INSTALL) -m 0755 $(@D)/buzzerdriver/initScript $(TARGET_DIR)/etc/init.d/S99buzzer
+	$(INSTALL) -m 0755 $(@D)/fingerprint-driver/initScript $(TARGET_DIR)/etc/init.d/S98fingerprint
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/S97aesdchar.sh $(TARGET_DIR)/etc/init.d/S97aesdchar
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/drivertest.sh $(TARGET_DIR)/bin
+
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
