c5b5ede5b0e4a5b4fd52ed3654b63547af59be4c
Sarah Stephany <sast7580@colorado.edu>
2019-12-09 02:31:21 -0700

    updated package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,14 +5,14 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 54d276151cd24a93285e9e35a066b770e028af36
+AESD_ASSIGNMENTS_VERSION = a79d7fc8379d2384bbcf88c8e31d5078d5e289ad
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = buzzer-driver/
-AESD_ASSIGNMENTS_MODULE_SUBDIRS = fingerprint-driver/
-AESD_ASSIGNMENTS_MODULE_SUBDIRS = gsm-driver/
-AESD_ASSIGNMENTS_MODULE_SUBDIRS = rfid-driver/
+#AESD_ASSIGNMENTS_MODULE_SUBDIRS = fingerprint-driver/
+#AESD_ASSIGNMENTS_MODULE_SUBDIRS = gsm-driver/
+#AESD_ASSIGNMENTS_MODULE_SUBDIRS = nrf/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -21,9 +21,9 @@ endef
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/buzzerdriver/initScript $(TARGET_DIR)/etc/init.d/S99buzzer
-	$(INSTALL) -m 0755 $(@D)/fingerprint-driver/initScript $(TARGET_DIR)/etc/init.d/S98fingerprint
+#	$(INSTALL) -m 0755 $(@D)/fingerprint-driver/initScript $(TARGET_DIR)/etc/init.d/S98fingerprint
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/S97aesdchar.sh $(TARGET_DIR)/etc/init.d/S97aesdchar
-	$(INSTALL) -m 0755 $(@D)/S98lddmodules.sh $(TARGET_DIR)/etc/init.d/S98lddmodules
+#	$(INSTALL) -m 0755 $(@D)/nrf/initScript $(TARGET_DIR)/etc/init.d/S98nrf
 
 endef
 
