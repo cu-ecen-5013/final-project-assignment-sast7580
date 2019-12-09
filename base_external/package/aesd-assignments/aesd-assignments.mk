073b7758a7b69974141412d95cba39f22f10a4d4
Sarah Stephany <sast7580@colorado.edu>
2019-12-09 03:41:43 -0700

    added flags

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,9 +5,10 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = eea1a02e283787e85b9544c6a750edf0de7fdfe2
+AESD_ASSIGNMENTS_VERSION = 09c84650ce674f393534774b713c86541cb1a450
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) - l/usr/local/lib -lwiringPi -lwiringPiDev -lpthread -lm -lcrypt -lrt
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = buzzer-driver/
 #AESD_ASSIGNMENTS_MODULE_SUBDIRS = fingerprint-driver/
@@ -15,7 +16,7 @@ AESD_ASSIGNMENTS_MODULE_SUBDIRS = buzzer-driver/
 #AESD_ASSIGNMENTS_MODULE_SUBDIRS = nrf/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) LDFLAGS=$(AESD_ASSIGNMENTS_LDFLAGS) all
 endef
 
 #TODO: Add required executables or scripts below
