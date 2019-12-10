e287641e31939cb78836f22b862b860d217cd532
Sarah Stephany <sast7580@colorado.edu>
2019-12-10 13:02:18 -0700

    trying to get uart enabled

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,10 +5,10 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 75f122d61f2cd3a7c9d4f85462807f0f34bc23b3
+AESD_ASSIGNMENTS_VERSION = 0f430185ffe1234a1f28ad390551a74f15c5f48b
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD= git
-AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -lwiringPi -lwiringPiDev -lpthread -ldialog -ltinfo -lncurses -lm -lcrypt -lrt
+AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -lwiringPi -lwiringPiDev -lpthread -lm -lcrypt -lrt
 AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
@@ -20,7 +20,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/buzzer-init.sh $(TARGET_DIR)/etc/init.d/S99buzzer
 	$(INSTALL) -m 0755 $(@D)/fingerprintModule $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/buzzerModule $(TARGET_DIR)/usr/bin
-
+	$(INSTALL) -m 0755 $(@D)/terminal.sh $(TARGET_DIR)/etc/init.d/S98terminal
 endef
 
 $(eval $(kernel-module))
