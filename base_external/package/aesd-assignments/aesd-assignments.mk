18958472b897081487b9a2e8346c59b81c6abd03
Sarah Stephany <sast7580@colorado.edu>
2019-12-11 09:52:04 -0700

    after clean build

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 3e937a6402bef7b123e42d6327d86c03aae73def
+AESD_ASSIGNMENTS_VERSION = 825f61a8e63659b9ee33f19853e642b49eb2e983
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-assignment-MacRush7.git
 AESD_ASSIGNMENTS_SITE_METHOD= git
 AESD_ASSIGNMENTS_LDFLAGS = $(LDFLAGS_FOR_BUILD) -lwiringPi -lwiringPiDev -lpthread -lm -lcrypt -lrt
